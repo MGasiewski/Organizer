72ffbe4148c8bfc635bab042841f925e21569957
Matthew Gasiewski <matthewjames84@gmail.com>
2023-08-27 14:39:21 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from './store';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Organizer';
+  stores!: Store[];
+  items!: Item[];
 }
